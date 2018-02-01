21b9f021814f4e37f09b8ae38a8cfc8c036d377b
Astaech <thomasobigand@aol.com>
2018-02-01 18:14:27 +0100

    create wordlist class

--- wordlist.rb
+++ wordlist.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/ruby
 #made by astaech
 
 module Cupspeller
@@ -18,22 +17,21 @@ module Cupspeller
       @@MAX_WRD.times { @word_list.push list[rand lines] }
     end
 
+    def gameplay
+      time = Time.new
+      resp = ''
+      @word_list.each do |word|
+        until resp == word do
+          puts word
+          resp = gets[0..-2]
+        end
+      end
+      Time.new - time
+    end
+
     def to_s
       @word_list.inspect
     end
 
   end
 end
-
-wl = Cupspeller::WordList.new
-
-resp = ''
-
-time = Time.new
-wl.word_list.each do |word|
-  until resp == word do
-    puts word
-    resp = gets[0..-2]
-  end
-end
-puts 'bravo : ' + (Time.new - time).to_s + 's'
